cb942db68dacb8b23345c35d9f2bec944cbf87d2
Katrin <nanivskaya_ek@mail.ru>
2019-01-13 16:29:23 +0300

    add a calendar

---
+++ Scheduler/Controllers/ChildController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scheduler.Controllers
+{
+    public class ChildController : Controller
+    {
+        // GET: Child
+        public ActionResult Workplace()
+        {
+            return View();
+        }
+
+        public ActionResult MainInformation()
+        {
+            return PartialView();
+        }
+    }
+}
